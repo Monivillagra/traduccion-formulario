c46e8a1943d906cea582b79504cc05e81de73bbe
Monivillagra <villagra.ramirez@gmail.com>
2016-10-24 22:46:31 -0300

    traduciendo el formulario al espanglish

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function traductor(){
+	document.getElementById('form-signin-heading').innerHTML="Por favor iniciar sesión";
+	document.getElementById('inputEmail').placeholder="Correo Electrónico";
+	document.getElementById('inputPassword').placeholder="Contraseña";
+	document.querySelector('.checkbox span').innerHTML="Recordar datos";
+	document.querySelector('button').textContent="Iniciar Sesión";
+}
